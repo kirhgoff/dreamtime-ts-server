1d2284e967e570691d3073d8fe4c017dc2d68532
Kirill Lastovirya <kirill.lastovirya@jora.com>
2021-05-28 20:20:09 +1000

    First commit

---
+++ src/config/database.ts
@@ -0,0 +1,15 @@
+import { ConnectionOptions } from "typeorm";
+import {User} from '../models'
+
+const config: ConnectionOptions = {
+  type: "postgres",
+  host: process.env.POSTGRES_HOST || "localhost",
+  port: Number(process.env.POSTGRES_PORT) || 5432,
+  username: process.env.POSTGRES_USER || "postgis",
+  password: process.env.POSTGRES_PASSWORD || "postgis",
+  database: process.env.POSTGRES_DB || "postgis",
+  entities: [User],
+  synchronize: true,
+};
+
+export default config;
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import "reflect-metadata";
+import express, { Application } from "express";
+import { createConnection } from "typeorm";
+import morgan from "morgan";
+
+import Router from "./routes";
+import dbConfig from "./config/database";
+
+const PORT = process.env.PORT || 8000;
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(express.static("public"));
+
+app.use(Router);
+
+createConnection(dbConfig)
+  .then(_connection => {
+    app.listen(PORT, () => {
+      console.log("Server is running on port", PORT);
+    });
+  })
+  .catch(err => {
+    console.log("Unable to connect to db", err);
+    process.exit(1);
+  });
